0e526e68c6c267ac1190667978f050eea24de724
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2022-03-13 19:55:37 +0000

    Added bad implementation of direction indicator

---
+++ components/AppDirectionIndicator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ direction}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useSensorsStore } from "../store/sensors";
+
+export default defineComponent({
+  setup () {
+    const sensors = useSensorsStore();
+
+    const { roll } = storeToRefs(sensors);
+
+    const direction = computed(() => {
+      // normalise the roll from [-90 to 90] to [0 to 360]
+      const rollDeg = (roll.value + 90) % 360;
+
+      return rollDeg;
+    });
+
+    return {
+      direction
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
